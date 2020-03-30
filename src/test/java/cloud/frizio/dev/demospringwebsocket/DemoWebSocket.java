ffe625f120a31c24cbed226912b690d613ff0a0a
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2020-03-30 16:18:48 +0200

    Add jupiter and test class

---
+++ src/test/java/cloud/frizio/dev/demospringwebsocket/DemoWebSocket.java
@@ -0,0 +1,13 @@
+package cloud.frizio.dev.demospringwebsocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoWebSocket {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
