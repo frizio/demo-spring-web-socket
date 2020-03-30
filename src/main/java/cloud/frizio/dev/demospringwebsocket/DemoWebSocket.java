d3f46ddc405a944c3ad79fe6b8f3003fba78b930
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2020-03-30 16:17:32 +0200

    first commit

---
+++ src/main/java/cloud/frizio/dev/demospringwebsocket/DemoWebSocket.java
@@ -0,0 +1,13 @@
+package cloud.frizio.dev.demospringwebsocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoWebSocket {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoWebSocket.class, args);
+	}
+
+}
