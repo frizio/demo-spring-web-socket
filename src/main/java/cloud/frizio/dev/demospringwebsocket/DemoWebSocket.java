0a1071ea42bf4e333d6e36a76baa1c338a60c85e
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2020-03-31 10:49:18 +0200

    Code refactor

--- src/main/java/cloud/frizio/dev/demospringwebsocket/DemoWebSocket.java
+++ src/main/java/cloud/frizio/dev/demospringwebsocket/DemoWebSocket.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoWebSocket {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoWebSocket.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(DemoWebSocket.class, args);
+  }
 
 }
